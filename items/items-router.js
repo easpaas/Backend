88af1469318c15af9cc6acc2a3b803cb10607b0b
evan <aspaasevan@gmail.com>
2020-05-26 14:12:09 -0700

    fixing server 500 on get items

--- items/items-router.js
+++ items/items-router.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get("/", (req, res) => {
   Items.find()
     .then(items => {
-      res.json(items);
+      res.status(200).json({data: items});
     })
     .catch(err => {
       res.status(500).json({ message: "Failed to get Items" });
